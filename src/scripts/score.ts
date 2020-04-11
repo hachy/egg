56d093a47534619643f2fac389125f28407defe9
hachy <1613863+hachy@users.noreply.github.com>
2020-04-11 13:56:55 +0900

    fix score count function

--- src/scripts/score.ts
+++ src/scripts/score.ts
@@ -4,7 +4,11 @@ export class Score {
   static el = document.getElementById('score') as HTMLSpanElement;
 
   static count(i: number): void {
-    Global.score += i;
+    if (i === 0) {
+      Global.score = 0;
+    } else {
+      Global.score += i;
+    }
     Score.el.textContent = `${Global.score}`;
   }
 }
